4ed364c0174c23bfa50a6b7420fa2595e1316345
Muhammad Ramdan <muhammadramdan.0512@gmail.com>
2023-08-24 21:17:09 +0700

    menambahhkan file index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index PHP</title>
+</head>
+<body>
+    <h3>Coba Index php</h3>
+    <ul>
+        <li>test 1 index</li>
+    </ul>
+</body>
+</html>
